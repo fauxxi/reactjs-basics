f9f5300708ed40dcf1cd35acaba587f01658cda9
fauzisan <fauziarifsanjaya@yahoo.com>
2018-08-10 01:48:12 +0200

    reactjs helloo

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
-console.log("heyhoo!!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+
+  render(){
+    return (
+    <div>
+      <h1>Hola from the other sideeee!</h1>
+    </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById("app"));
