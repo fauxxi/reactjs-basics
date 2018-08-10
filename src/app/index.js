9dc5f72ec2aebff0c09d015b8296bd3e0d04d548
fauzisan <fauziarifsanjaya@yahoo.com>
2018-08-10 02:10:00 +0200

    reactjs dynamic content

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
 class App extends React.Component {
 
   render(){
     return (
-    <div>
-      <h1>Hola from the other sideeee!</h1>
+    <div className="container">
+      <Header/>
+      <h1>HEEEEEYYY {10*10}</h1>
+      <Home/>
     </div>
     );
   }
